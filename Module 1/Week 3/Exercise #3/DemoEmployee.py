8602aaf31b6912d665fa541dc853b8fc62457f39
v-adrian <97440298+v-adrian@users.noreply.github.com>
2022-03-16 13:52:07 +0800

    Create DemoEmployee.py

---
+++ Module 1/Week 3/Exercise #3/DemoEmployee.py
@@ -0,0 +1,12 @@
+# Vonn Adrian C. Jutar - A121
+# Testing Employee.py
+
+from Employee import *
+
+employee = Employee("John Smith", 45000)
+manager = Manager("Jane Doe", 60000, "Widgets")
+executive = Executive("Weird Guy", 90000, "Thingies")
+
+print(repr(employee))
+print(repr(manager))
+print(repr(executive))
