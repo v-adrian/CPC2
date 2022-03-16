ece02e70c50859c26854d4e3b56b99d8fdb3d02a
v-adrian <97440298+v-adrian@users.noreply.github.com>
2022-03-16 13:52:51 +0800

    Add files via upload

---
+++ Module 1/Week 3/Exercise #2/Question.py
@@ -0,0 +1,37 @@
+# Vonn Adrian C. Jutar - A121
+
+# Superclass
+class Question:
+    def __init__(self):
+        self._question = ""
+        self._answer = ""
+
+    def set_question(self, question):
+        self._question = question
+
+    def set_answer(self, answer):
+        self._answer = answer
+
+    def check_answer(self, response):
+        return response == self._answer
+
+    def display_question(self):
+        print(self._question)
+
+# Subclass
+class MultipleChoice(Question):
+    def __init__(self):
+        super().__init__()
+        self._choices = []
+
+    def add_choice(self, choice, correct):
+        self._choices.append(choice)
+        if correct:
+            choiceString = str(len(self._choices))
+            self.set_answer(choiceString)
+
+    def display_question(self):
+        super().display_question()
+        for i in range(len(self._choices)):
+            choiceNumber = i + 1
+            print("%d: %s" % (choiceNumber, self._choices[i]))
