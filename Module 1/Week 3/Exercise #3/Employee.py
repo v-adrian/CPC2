7f7cb44683c13caf9917061504f6152ea61c4d1c
v-adrian <97440298+v-adrian@users.noreply.github.com>
2022-03-16 13:53:07 +0800

    Add files via upload

---
+++ Module 1/Week 3/Exercise #3/Employee.py
@@ -0,0 +1,26 @@
+# Vonn Adrian C. Jutar - A121
+
+class Employee:
+    def __init__(self, name, salary):
+        self._name = name
+        self._salary = salary
+
+    def __repr__(self):
+        return "{} has a salary of {:.02f}".format(self._name, self._salary)
+
+
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self._department = department
+
+    def __repr__(self):
+        return "{} has a salary of {:.02f} and manages the {} department".format(self._name, self._salary, self._department)
+
+
+class Executive(Manager):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary, department)
+    
+    def __repr__(self):
+        return "{} has a salary of {:.02f} and is the executive for the {} department".format(self._name, self._salary, self._department)
