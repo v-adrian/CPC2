014bff046c4a3e3eb97a23cd55d7920ac18c7b4f
v-adrian <97440298+v-adrian@users.noreply.github.com>
2022-03-29 19:28:01 +0800

    Add files via upload

---
+++ Module 2/Week 7/HTML and Python.py
@@ -0,0 +1,22 @@
+import webbrowser as wb
+
+message = '''
+<!DOCTYPE html>
+<html>
+    <title>Exercise #5 </title>
+
+    <head> 
+        <h1>Welcome to my first HTML Program using Python!</h1> 
+    </head>
+
+    <body> 
+        <h2>Vonn Adrian C. Jutar- A121</h2>
+        <p>Thank you!</p> 
+    </body>
+'''
+
+f = open('Exercise #5.html', 'w')
+f.write(message)
+f.close()
+
+wb.open_new_tab('Exercise #5.html')
